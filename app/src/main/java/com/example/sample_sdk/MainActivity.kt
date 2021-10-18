e33fb70bc1148058d2add88e184732336fa47855
Patel-Preet <pp225622@gmail.com>
2021-10-18 13:11:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sample_sdk/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sample_sdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.my_sdk_library.SdkClass
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        SdkClass("aa")
+    }
+}
---
+++ my-sdk-library/src/main/java/com/example/my_sdk_library/SdkClass.kt
@@ -0,0 +1,4 @@
+package com.example.my_sdk_library
+
+class SdkClass
+    constructor(val sec : String)
