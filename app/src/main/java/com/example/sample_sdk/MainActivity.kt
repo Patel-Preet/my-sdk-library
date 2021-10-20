77a2523416b39047176c24f7f0097b977c197afd
Patel-Preet <pp225622@gmail.com>
2021-10-20 11:14:00 +0800

    SdkClass2.kt with public and private methods

--- app/src/main/java/com/example/sample_sdk/MainActivity.kt
+++ app/src/main/java/com/example/sample_sdk/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.sample_sdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.my_sdk_library.SdkClass
+import com.example.my_sdk_library.SdkClass2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        SdkClass("aa")
+        SdkClass("test")
+
+        SdkClass2.SdkPublicFun()
+        Toast.makeText(this, SdkClass2.SdkPublicVariable, Toast.LENGTH_LONG).show()
     }
 }
---
+++ my-sdk-library/src/main/java/com/example/my_sdk_library/SdkClass2.kt
@@ -0,0 +1,15 @@
+package com.example.my_sdk_library
+
+object SdkClass2 {
+
+    public var SdkPublicVariable:String = "SdkPublicString"
+    private var SdkPrivateVariable:String = "SdkPrivateString"
+
+    public fun SdkPublicFun(){
+
+    }
+
+    private fun SdkPrivateFun(){
+
+    }
+}
